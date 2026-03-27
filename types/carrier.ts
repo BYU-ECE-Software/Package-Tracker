7db7470fa4fd559c03b9abba7ee012f69245d081
Michelle Johanson <mjohans0@et.byu.edu>
2026-03-27 16:38:51 -0600

    update order to dropdown types

--- types/carrier.ts
+++ types/carrier.ts
@@ -2,14 +2,17 @@ export interface Carrier {
   id: string;
   name: string;
   isActive: boolean;
+  order: number;
 }
 
 export interface CreateCarrierRequest {
   name: string;
   isActive?: boolean;
+  order?: number;
 }
 
 export interface UpdateCarrierRequest {
   name?: string;
   isActive?: boolean;
+  order?: number;
 }
--- types/sender.ts
+++ types/sender.ts
@@ -2,14 +2,17 @@ export interface Sender {
   id: string;
   name: string;
   isActive: boolean;
+  order: number;
 }
 
 export interface CreateSenderRequest {
   name: string;
   isActive?: boolean;
+  order?: number;
 }
 
 export interface UpdateSenderRequest {
   name?: string;
   isActive?: boolean;
+  order?: number;
 }
